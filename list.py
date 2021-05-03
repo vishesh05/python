8178f7ce89cbdf8f91132e90a8f83ec1c9a25ca8
Vishesh Mishra <visheshmishra04@gmail.com>
2021-05-04 00:45:07 +0530

    Day 1

---
+++ list.py
@@ -0,0 +1,13 @@
+#making List
+
+li=["Vishesh", "Viv", "Aditya"]
+new=[["Hey", "There"],["Hello"]]
+
+print(li)
+li.append("Hello")
+li.insert(1, "One")
+print(li)
+li.extend([6, "new", "Multiple"])
+li.remove("new")
+print(li)
+print(new[0][1])
