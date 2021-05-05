abcd7e7f012475431118e0efa2cf55da9d0b2ba2
Vishesh Mishra <41970896+vishesh05@users.noreply.github.com>
2021-05-05 23:26:41 +0530

    Delete dicprob.py

--- dicprob.py
+++
@@ -1,7 +0,0 @@
-#create a dictionary and take a input from the user and return the meaning of the word from the dictionary
-
-dic={"See You Again":"Wiz khalifa", "Lovely":"Billie Eilesh", "The Nights":"Avichi"}
-
-Search=input()
-
-print(dic[Search])
