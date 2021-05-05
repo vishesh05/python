ebfb1e9fd7a94ac47ee9f1917d31f66b7cda894c
Vishesh Mishra <41970896+vishesh05@users.noreply.github.com>
2021-05-05 23:29:14 +0530

    Day 1 Codes

---
+++ Day1/dic.py
@@ -0,0 +1,14 @@
+#dicti
+
+dic={"Hey":"There", "Good":"MOrning", "Love":"You"}
+print(dic)
+dic1={}
+dic1[0]="Vishesh"
+dic1[2]="is"
+dic1[3]=0
+print(dic1)
+dic1[3]="Hero"
+print(dic1)
+del dic["Good"]
+print(dic)
+
---
+++ Day1/list.py
@@ -0,0 +1,5 @@
+li=[]
+print("ENter a li")
+for i in range(1,5):
+    li=int(input())
+print(li)
